7d580ff3e53076f251dbad0dae60b219bf8ba12d
Jonas-Aesthetic <125463648+Jonas-Aesthetic@users.noreply.github.com>
2026-04-10 22:45:19 -0300

    Update agenda.php

--- estrutura do projeto/agenda/api/agenda.php
+++ estrutura do projeto/agenda/api/agenda.php
@@ -2,6 +2,7 @@
 
 // RA: 202510196 
 // feito por Jonas Berlanda
+// 3 classes utilizadas, compromisso, banco e agendamento
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
